e1a6399f4481ca750a6787050d1c75dd5212acec
Ronald Nestor <ronald4261@hotmail.com>
2013-09-24 16:19:06 -0500

    primer commit

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-09-24 14:11:21 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2013-09-24	14:11:21	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
